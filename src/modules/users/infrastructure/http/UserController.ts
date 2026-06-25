8987766b2ef65e405a2a5162dc2cd7499105beb6
Luiz Jurazek <luizjurazek@gmail.com>
2026-06-25 17:14:41 -0300

    refactor: integrate express next function into user creation flow for centralized error handling

--- src/modules/users/infrastructure/http/UserController.ts
+++ src/modules/users/infrastructure/http/UserController.ts
@@ -1,16 +1,17 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import { CreateUserUseCase } from '../../application/create-user/CreateUserUseCase';
 
 export class UserController {
   constructor(private readonly createUserUseCase: CreateUserUseCase) {}
 
-  async create(req: Request, res: Response): Promise<Response> {
+  async create(req: Request, res: Response, next: NextFunction): Promise<Response | void> {
     try {
       const { name, email } = req.body;
       const user = await this.createUserUseCase.execute({ name, email });
+      
       return res.status(201).json(user);
-    } catch (error: any) {
-      return res.status(400).json({ error: error.message });
+    } catch (error) {
+      next(error);
     }
   }
 }
--- src/modules/users/infrastructure/http/user.routes.ts
+++ src/modules/users/infrastructure/http/user.routes.ts
@@ -1,4 +1,4 @@
-import { Router } from 'express';
+import { Router, NextFunction } from 'express';
 import { makeUserController } from './factories/UserControllerFactory';
 import { validateDto } from '../../../../shared/infra/http/middlewares/validation.middleware';
 import { CreateUserDTO } from '../../application/create-user/dto';
@@ -6,7 +6,7 @@ import { CreateUserDTO } from '../../application/create-user/dto';
 const userRoutes = Router();
 const userController = makeUserController();
 
-userRoutes.post('/', validateDto(CreateUserDTO), (req, res) => userController.create(req, res));
+userRoutes.post('/', validateDto(CreateUserDTO), (req, res, next: NextFunction) => userController.create(req, res, next));
 
 export { userRoutes };
 
