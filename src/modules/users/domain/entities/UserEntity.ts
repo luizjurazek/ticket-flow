012473189a284810fef9bc16a7aad961817f6eca
Luiz Jurazek <luizjurazek@gmail.com>
2026-06-25 15:34:05 -0300

    feat: update user entity with new methods

--- src/modules/users/domain/entities/UserEntity.ts
+++ src/modules/users/domain/entities/UserEntity.ts
@@ -1,12 +1,28 @@
-import { Ticket } from "../../../tickets/domain/Ticket.entity";
+import { Ticket } from "../../../tickets/domain/TicketEntity";
+import { v4 as uuidv4 } from "uuid";  
+
+export interface ICreateUserData {
+  name: string;
+  email: string;
+}
 
 export class User {
   constructor(
-    public id: string,
     public name: string,
     public email: string,
+    public id: string = uuidv4(),
     public tickets: Ticket[] = [],
-    public createdAt: Date,
-    public updatedAt: Date,
+    public createdAt: Date = new Date(),
+    public updatedAt: Date = new Date(),
   ) {}
+
+  static create(userData: ICreateUserData): User {
+    return new User(userData.name, userData.email)
+  }
+
+  update(userData: ICreateUserData): void {
+    this.name = userData.name;
+    this.email = userData.email;
+    this.updatedAt = new Date();
+  }
 }
