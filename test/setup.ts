bf8543b6f266293fc31faeeb76122baa05a43257
Luiz Jurazek <luizjurazek@gmail.com>
2026-06-26 11:53:24 -0300

    chore: update dotenv configuration in test setup to enable quiet mode

--- test/setup.ts
+++ test/setup.ts
@@ -1,7 +1,7 @@
 import 'reflect-metadata';
 import dotenv from 'dotenv';
 
-dotenv.config({ path: '.env.test' });
+dotenv.config({ path: '.env.test', quiet: true });
 
 // Default value to avoid code breaking
 process.env.GEMINI_API_KEY = 'fake-key-for-testing';
