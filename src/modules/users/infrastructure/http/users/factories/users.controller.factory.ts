942cb83ec5161ea79384b11f23c184d59f407352
Luiz Jurazek <luizjurazek@gmail.com>
2026-06-26 14:11:38 -0300

    fix: rename detete-user folder to delete-user

--- src/modules/users/infrastructure/http/users/factories/users.controller.factory.ts
+++ src/modules/users/infrastructure/http/users/factories/users.controller.factory.ts
@@ -5,7 +5,7 @@ import { CreateUserUseCase } from '@/modules/users/application/create-user/creat
 import { UpdateUserUseCase } from '@/modules/users/application/update-user/update-user.usecase';
 import { GetUsersUseCase } from '@/modules/users/application/get-users/get-users.usecase';
 import { GetUserByIdUseCase } from '@/modules/users/application/get-user-by-id/get-user-by-id.usecase';
-import { DeleteUserUseCase } from '@/modules/users/application/detete-user/delete-user.usecase';
+import { DeleteUserUseCase } from '@/modules/users/application/delete-user/delete-user.usecase';
 
 export function makeUsersController(): UsersController {
   const userRepository = new PrismaUserRepository(prisma);
