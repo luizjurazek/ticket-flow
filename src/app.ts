d67c41a0badd4b339c5b85f09f21c43609f98fd1
Luiz Jurazek <luizjurazek@gmail.com>
2026-06-28 10:15:43 -0300

    feat: integrate SwaggerModule into the application for enhanced API documentation setup

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import cors from 'cors';
+import 'reflect-metadata';
 import { globalRouter } from './shared/infra/http/routes/routes';
 import { errorHandler } from './shared/infra/http/middlewares/error-handler.middleware';
 import { httpLogger } from '@/shared/infra/http/middlewares/http-logger.middleware';
 import { requestContext } from '@/shared/infra/http/middlewares/request-context.middleware';
+
+import { SwaggerModule } from '@/shared/infra/swagger/swagger.module';
 
 const app = express();
 
@@ -14,5 +17,7 @@ app.use(requestContext);
 app.use(httpLogger);
 app.use(globalRouter);
 app.use(errorHandler);
+
+SwaggerModule.setup(app);
 
 export { app };
