375d6dd75e0143e0c65c0c4264d828a9fe5a930b
Luiz Jurazek <luizjurazek@gmail.com>
2026-06-28 19:49:41 -0300

    feat: implement CreateTicketUseCase for ticket creation and classification logic

---
+++ src/modules/tickets/applitcation/create-ticket/create-ticket.usecase.ts
@@ -0,0 +1,25 @@
+import { ITicketRepository } from '@/modules/tickets/domain/repositories/ticket.repository.interface';
+import { Ticket } from '@/modules/tickets/domain/entities/ticket.entity';
+import { ICreateTicketInput } from '@/modules/tickets/domain/entities/ticket.entity';
+import { IClassifyTicketService } from '@/modules/tickets/domain/services/classify-ticket.service.interface';
+
+export class CreateTicketUseCase {
+  constructor(
+    private readonly ticketRepository: ITicketRepository,
+    private readonly ticketClassifier: IClassifyTicketService,
+  ) {}
+
+  async execute(data: ICreateTicketInput): Promise<Ticket> {
+    const classification = await this.ticketClassifier.classify(data.message);
+    const ticket = Ticket.create({
+      message: data.message,
+      userId: data.userId,
+      manuallyReview: classification.manuallyReview,
+      channel: classification.channel,
+      priority: classification.priority,
+    });
+
+    const createdTicket = await this.ticketRepository.create(ticket);
+    return createdTicket;
+  }
+}
